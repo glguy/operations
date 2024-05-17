388ea5f97cc111304298e963dd3b4e67d30194ee
Eric Mertens <emertens@galois.com>
2024-05-17 11:41:48 -0700

    Try out quantified constraints

--- src/Object.hs
+++ src/Object.hs
@@ -1,4 +1,5 @@
 {-# Language ExistentialQuantification #-}
+{-# Language QuantifiedConstraints #-}
 {-# Language MultiParamTypeClasses, FlexibleInstances, FlexibleContexts #-}
 {-# Language RankNTypes #-}
 {-# Language ConstraintKinds, DataKinds #-}
@@ -12,78 +13,50 @@
 module Object
   ( Object(Object)
   , type (⊆)
-  , type (∈)
-  , type All
-  , type Use
 
   , upcast
 
-  , using1, usingN -- usingA
-  , map1, mapN -- mapA
+  , using
+
+  , type (&)
+  , Null
 
   ) where
 
-import TypeListOps
 import Data.Kind
-import Data.Constraint ( (:-), top, (&&&), weaken1, weaken2, (\\))
 import Control.Category (id, (.))
 import Prelude hiding (id, (.))
 
-data Object cs = forall a. All cs a => Object a
+data Object c = forall a. c a => Object a
 
-instance '[Show] ⊆ cs => Show (Object cs) where
-  showsPrec p o = showParen (p >= 11)
-                $ showString "Object "
-                . using1 @Show o (showsPrec 11)
+instance (Show ⊆ cs) => Show (Object cs) where
+  show o = objectShowsPrec 11 o ""
+  showsPrec = objectShowsPrec
 
+  showList [] s = "[]" ++ s
+  showList (x:xs) s = '[' : objectShowsPrec 0 x (foldr aux (']':s) xs)
+    where
+      aux :: Object cs -> ShowS
+      aux y ys = ',' : objectShowsPrec 0 y ys
 
-------------------------------------------------------------------------
-
-usingN :: forall cs ds r. cs ⊆ ds => Object ds -> (forall a. All cs a => a -> r) -> r
-usingN (Object (x :: a)) f = f x \\ pickN @cs @ds @a
-
-
+objectShowsPrec :: (Show ⊆ cs) => Int -> Object cs -> ShowS
+objectShowsPrec p (Object o)
+     = showParen (p >= 11)
+     $ showString "Object "
+     . showsPrec 11 o
 
 upcast :: forall cs ds. cs ⊆ ds => Object ds -> Object cs
-upcast o = usingN @cs o Object
+upcast o = using @cs o Object
 
-using1 :: forall c cs r. c ∈ cs => Object cs -> (forall a. c a => a -> r) -> r
-using1 = usingN @'[c]
-
--- usingA :: forall cs r. Object cs -> (forall a. All cs a => a -> r) -> r
--- usingA = usingN @cs
+using :: forall c cs r. c ⊆ cs => Object cs -> (forall a. c a => a -> r) -> r
+using (Object o) f = f o
 
 ------------------------------------------------------------------------
 
-mapN :: forall cs ds. cs ⊆ ds => Object ds -> (forall a. All cs a => a -> a) -> Object ds
-mapN (Object (x :: a)) f = Object (f x) \\ pickN @cs @ds @a
+type (cs :: Type -> Constraint) ⊆ (ds :: Type -> Constraint) = (forall a. ds a => cs a) :: Constraint
 
+class    (c1 a, c2 a) => (c1 & c2) a
+instance (c1 a, c2 a) => (c1 & c2) a
 
-map1 :: forall c cs. c ∈ cs => Object cs -> (forall a. c a => a -> a) -> Object cs
-map1 = mapN @'[c]
-
--- mapA :: forall cs. Object cs -> (forall a. All cs a => a -> a) -> Object cs
--- mapA = mapN @cs
-
-------------------------------------------------------------------------
-
-class                     c ∈ cs        where pick1 :: All cs a :- c a
-instance {-# OVERLAPS #-} c ∈ (c ': cs) where pick1 = weaken1
-instance c ∈ cs =>        c ∈ (d ': cs) where pick1 = pick1 @c @cs . weaken2
-
-------------------------------------------------------------------------
-
-type family (⊆´) cs ds :: Constraint where
-  (c ': cs) ⊆´ ds = (c ∈ ds, cs ⊆ ds)
-  '[]       ⊆´ ds = ()
-
-class cs ⊆´ ds => cs ⊆ ds where
-  pickN :: forall a. All ds a :- All cs a
-
-instance '[] ⊆ ds where
-  pickN = top
-
-instance ((c ': cs) ⊆´ ds) => (c ': cs) ⊆ ds where
-  pickN :: forall a. All ds a :- All (c ': cs) a
-  pickN = pick1 @c  @ds @a
-      &&& pickN @cs @ds @a
+class    Null a
+instance Null a
